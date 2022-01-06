1b36250b01657293f8cb03027565e811e67584cc
jia.jiuan.tsai@gmail.com <jia.jiuan.tsai@gmail.com>
2022-01-06 19:28:26 +0100

    Create RectangleClass and methods required

--- Object_Oriented_Shape/Program.cs
+++ Object_Oriented_Shape/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Object_Oriented_Shape;
 
-//
+Console.WriteLine();
+
+Rectangle rectangle = new Rectangle(2,4);
+Console.WriteLine(rectangle);
+rectangle.SayHello();
+
+Console.WriteLine();
---
+++ Object_Oriented_Shape/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Shape
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+       // public Rectangle(): base() { }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width ;
+            Height = height;
+        }
+
+        public Rectangle() { }
+
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+
+        public override double GetPerimeter()
+        {
+            return Width * 2 + Height * 2;
+        }
+
+        public override string ToString()
+        {
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.AppendLine($"This is a {this.GetType().Name} with height {Height} and width {Width}. Area is {GetArea()} and perimeter is {GetPerimeter()}");
+            string newString = $"This is a {this.GetType().Name} with height {Height} and width {Width}. Area is {GetArea()} and perimeter is {GetPerimeter()}";
+            return newString;
+        }
+
+
+        public override void ScreamHello()
+        {
+            Console.WriteLine($"{this.GetType().Name} RecClass ScreamHello {Width}!");
+        }
+    }
+}
